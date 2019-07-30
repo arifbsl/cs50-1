b1c210da26d31e2d02a3331c470b54724111c780
jsumner48 <50372857+jsumner48@users.noreply.github.com>
2019-07-30 11:09:03 -0400

    Update mario.py

--- pset6/mario/more/mario.py
+++ pset6/mario/more/mario.py
@@ -2,8 +2,7 @@ import cs50
 
 # asks user for height for pyramid and also makes sure that height is within bounds
 while True:
-    print("Enter height of the pyramid (please pick a number between 1 and 8, inclusive)")
-    Height = cs50.get_int()
+    Height = cs50.get_int("Enter height of the pyramid (please pick a number between 1 and 8, inclusive)")
     if Height >= 1 and Height <= 8:
         break
 
